79f85187742106bc31f4d324331adc130b0ec773
karolina <mykolay20@gmail.com>
2025-02-06 12:55:06 +0200

    Day 20 move the snake

---
+++ Day_20/snake.py
@@ -0,0 +1,51 @@
+from turtle import Turtle, Screen
+STARTING_POSITION = [(0,0),(-20, 0),(-40, 0)]
+MOVE_DISTANCE = 20
+class Snake:
+
+    # Class attributes (shared by all instances)
+    # class_attribute = value
+
+    # Constructor method (initializes instance attributes)
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+
+
+    # Instance method
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+        pass
+
+    # Class method (decorator)
+    @classmethod
+    def class_method(cls):
+        # Method logic
+        pass
+
+    # Static method (decorator)
+    @staticmethod
+    def static_method():
+        # Method logic
+        pass
+
+
+
+
+
+
